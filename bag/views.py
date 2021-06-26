6aba6a4dc4ee5bb6f6a62cdecfa7ceb0fd8ff3ce
alandoherty95 <alandoherty95@gmail.com>
2021-06-26 12:14:49 +0000

    Implemented functionality for adjusting quantity of items from checkout bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ ALLOWS USERS TO ADD SPECIFIC PRODUCTS 
+    """ ALLOWS USERS TO ADD SPECIFIC PRODUCTS
     TO SHOPPING BAG INCLUDING QUANTITY OF ITEMS """
 
     quantity = int(request.POST.get('quantity'))
@@ -24,3 +24,18 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ ADJUST QUANTITY OF SPECIFIC ITEM """
+  
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
