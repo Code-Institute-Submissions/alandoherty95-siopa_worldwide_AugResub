9bab37ca884d260d89f21ba5a973954464a786c7
alandoherty95 <alandoherty95@gmail.com>
2021-06-27 14:03:49 +0000

    Added levels to determine type of toast to display

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from products.models import Product
