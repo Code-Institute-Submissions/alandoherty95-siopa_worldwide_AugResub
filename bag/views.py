20cf84798e1c98dbf1b7f171a00f143b1f2e02b3
alandoherty95 <alandoherty95@gmail.com>
2021-06-29 16:42:08 +0000

    Added shopping bag nofications when item is added or removed from bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -6,7 +6,7 @@ from products.models import Product
 
 
 def view_bag(request):
-    """ VIEW SHOWING CONTENTS OF SHOPPING BAG """
+    """ SHOWS ALL CONTENTS IN SHOPPING BAG """
 
     return render(request, 'bag/bag.html')
 
@@ -15,16 +15,17 @@ def add_to_bag(request, item_id):
     """ ALLOWS USERS TO ADD SPECIFIC PRODUCTS
     TO SHOPPING BAG INCLUDING QUANTITY OF ITEMS """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated quantity of {product.name} to {bag[item_id]}')
     else:
         bag[item_id] = quantity
-        messages.success(request, f'Successfully added {product.name} to shopping bag')
+        messages.success(request, f'Successfully added {product.name} to shopping bag.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -32,14 +33,17 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """ ADJUSTS QUANTITY OF SPECIFIC ITEM """
-  
+    
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated quantity of {product.name} to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from shopping bag.')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -48,8 +52,15 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ REMOVES SELECTED ITEM FROM SHOPPING BAG """
 
-    bag = request.session.get('bag', {})
-    bag.pop(item_id)
+    product = Product.objects.get(pk=item_id)
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        messages.info(request, f'Removed {product.name} from shopping bag.')
+        request.session['bag'] = bag
+        return redirect(reverse('view_bag'))
     
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+    except Exception as e:
+        messages.error(request, f'Error removing item. Error: {e}')
+        return HttpResponse(status=500)
