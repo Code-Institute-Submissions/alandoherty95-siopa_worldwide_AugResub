58e615c2d182a1a5a55d49b6f7ad446f78fa2378
alandoherty95 <alandoherty95@gmail.com>
2021-07-01 11:53:03 +0000

    Created forms.py in checkout folder, added placeholders to form fields for customer delivery details

---
+++ checkout/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        all_fields = ('full_name', 'email', 'phone_number',
+                      'street_address1', 'street_address2',
+                      'town_or_city', 'postcode', 'country',
+                      'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Adds placeholders and classes to form fields,
+        removes auto-generated labels
+        """
+        super().__init__(*args, **kwargs)
+
+        # Sets placeholders in each form field
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        # Sets autofocus to first field
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
