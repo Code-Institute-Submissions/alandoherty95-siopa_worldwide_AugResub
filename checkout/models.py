774513657c10044436115e0b85ce00935245d9c8
alandoherty95 <alandoherty95@gmail.com>
2021-07-01 17:03:32 +0000

    Installed django crispy forms for use on checkout page, added additonal information to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -13,17 +13,24 @@ class Order(models.Model):
     email = models.EmailField(max_length=200, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=30, null=False, blank=False)
-    postcode = models.CharField(max_length=22, null=True, blank=True)  # Not required
+    postcode = models.CharField(
+        max_length=22, null=True, blank=True)  # Not required
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=60, null=False, blank=False)
-    street_address2 = models.CharField(max_length=60, null=True, blank=True) # Not required
-    county = models.CharField(max_length=60, null=True, blank=True)  # Not required
-    date = models.DateTimeField(auto_now_add=True)  # Automatically sets date and time of order
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    street_address2 = models.CharField(
+        max_length=60, null=True, blank=True)  # Not required
+    county = models.CharField(
+        max_length=60, null=True, blank=True)  # Not required
+    date = models.DateTimeField(
+        auto_now_add=True)  # Automatically sets date and time of order
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
 
-    def create_order_number(self):
+    def _generate_order_number(self):
         """
         Creates a unique order number for each item using UUID
         """
@@ -36,7 +43,8 @@ class Order(models.Model):
         FREE_DELIVERY_THRESHOLD = €60
         Free delivery if order is more than €60
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum(
+            'lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -49,7 +57,7 @@ class Order(models.Model):
         Updates the order number
         """
         if not self.order_number:
-            self.order_number = self.create_order_number()
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -57,10 +65,15 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2,
+        null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
