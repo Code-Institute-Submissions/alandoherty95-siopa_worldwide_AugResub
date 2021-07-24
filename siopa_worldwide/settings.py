aae6b2e03fd7b9aa7ebd1b25032ead90d619624b
alandoherty95 <alandoherty95@gmail.com>
2021-07-24 12:11:02 +0000

    Added confirmation email body content, loaddata for categories and products

--- siopa_worldwide/settings.py
+++ siopa_worldwide/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,9 +121,9 @@ WSGI_APPLICATION = 'siopa_worldwide.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
 
 
