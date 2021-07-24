9f30aa86308b75e0d9ce05b288afdc6b1250032d
alandoherty95 <alandoherty95@gmail.com>
2021-07-24 10:23:49 +0000

    Imported login_required item for profile as well as adding, editing and deleting products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
@@ -71,6 +72,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Allows admin to add a product to the range """
     if request.method == 'POST':
@@ -93,6 +95,7 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ Allows admin to edit a product in the range """
     product = get_object_or_404(Product, pk=product_id)
@@ -117,6 +120,7 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Allows admin to delete a product from the range """
     product = get_object_or_404(Product, pk=product_id)
