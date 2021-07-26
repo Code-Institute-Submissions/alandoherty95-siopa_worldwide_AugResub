477b524f60d0f8e688655c4cf7b5cca42f59be2e
alandoherty95 <alandoherty95@gmail.com>
2021-07-26 16:33:12 +0000

    Deployment successful, installed boto3 and django-storages, freeze requirements.txt

--- siopa_worldwide/settings.py
+++ siopa_worldwide/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -175,6 +176,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Media files (Screenshots & Images)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'siopa-worldwide'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Stripe Payments Configuration
 FREE_DELIVERY_THRESHOLD = 40
