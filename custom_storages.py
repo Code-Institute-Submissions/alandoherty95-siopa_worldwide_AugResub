6bf17fab60f3e6c8aac293455d179ff62c700844
alandoherty95 <alandoherty95@gmail.com>
2021-07-27 13:51:21 +0000

    Added extra line to custom_storages.py for PEP8

--- custom_storages.py
+++ custom_storages.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
 # Storage for static files
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+
 
 # Storage for media files
 class MediaStorage(S3Boto3Storage):
