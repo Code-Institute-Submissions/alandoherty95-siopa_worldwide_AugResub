3c823256c61e24cf48ba19961ab497e1f917fa91
alandoherty95 <alandoherty95@gmail.com>
2021-07-27 15:47:41 +0000

    Added decimal to models.py in resolve issue with checkout submit

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
+from decimal import Decimal
 from django.conf import settings
 
 from django_countries.fields import CountryField
@@ -50,10 +51,10 @@ class Order(models.Model):
         FREE_DELIVERY_THRESHOLD = €60
         Free delivery if order is more than €60
         """
-        self.order_total = self.lineitems.aggregate(Sum(
-            'lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = Decimal(settings.STANDARD_DELIVERY_FEE)
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
