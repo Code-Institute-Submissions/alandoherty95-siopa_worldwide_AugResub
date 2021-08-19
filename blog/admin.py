b05393c98571e2568e246733b0fe667f18fbafc7
alandoherty95 <alandoherty95@gmail.com>
2021-08-19 11:16:19 +0000

    Created blog app, created blog.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import BlogPost
 
-
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-
-
-admin.site.register(BlogPost, BlogAdmin)
+# Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,3 @@
 from django.db import models
- 
- 
-class BlogPost(models.Model):
-   class Meta:
-       verbose_name = 'Blog Post'
- 
-   title = models.CharField(max_length=254, blank=False, null=False)
-   slug = models.SlugField(max_length=254, unique=True)
-   body = models.TextField(blank=False, null=False)
-   image = models.ImageField(blank=True, null=True)
-   date_created = models.DateTimeField(auto_now_add=True)
- 
-   def __str__(self):
-       return self.title
+
+# Create your models here.
