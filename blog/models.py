fc279e75616be3861e4a6275f4123c01eaab04f2
alandoherty95 <alandoherty95@gmail.com>
2021-08-20 15:53:04 +0000

    Created fixtures file for blog posts

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=254, blank=False, null=False)
     body = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
