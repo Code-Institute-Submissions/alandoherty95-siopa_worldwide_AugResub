65beaf4e328070c718c7d42db105e29c76aa8a77
alandoherty95 <alandoherty95@gmail.com>
2021-08-21 13:18:56 +0000

    Replace secret key in sttings.py, troubleshooting issue with blog in deployed version

--- siopa_worldwide/settings.py
+++ siopa_worldwide/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-p6do^rrh9$fc@%y9=@m+caqe9ijny#$_#q$cs1f7nri5&#_+h8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['siopa-worldwide.herokuapp.com', 'localhost']
 
