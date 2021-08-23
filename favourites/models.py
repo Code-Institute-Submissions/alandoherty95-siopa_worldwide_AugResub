fe7ad1ee9a4245e8caec411d2d75e0084bc2f7e4
alandoherty95 <alandoherty95@gmail.com>
2021-08-23 18:15:57 +0000

    Added 5 images for future products

--- favourites/models.py
+++ favourites/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 
-# FAVOURITES FORM
+# FAVOURITES FORM FOR CATEGORIES
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name  # Gets friendly name of category
+
+
+# FAVOURITES FORM FOR PRODUCTS
 class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
