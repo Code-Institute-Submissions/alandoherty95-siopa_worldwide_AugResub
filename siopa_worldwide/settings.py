ec4b1e14fcdee46431141aec129c1888f5e6fa89
alandoherty95 <alandoherty95@gmail.com>
2021-08-23 16:37:41 +0000

    Set heroku config:set DISABLE_COLLECTSTATIC=1, deploying to heroku now

--- siopa_worldwide/settings.py
+++ siopa_worldwide/settings.py
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'siopa_worldwide.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # Use as database settings, else use default setting
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
