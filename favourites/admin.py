2be967f1db7fffa109f9a188a84bde5a26386ff1
alandoherty95 <alandoherty95@gmail.com>
2021-08-23 18:22:10 +0000

    Added widget for future products

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+# ADMIN PRODUCT VIEW
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+# ADMIN CATEGORY VIEW
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
