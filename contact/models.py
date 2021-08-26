d9796fab42fc101842571d37e924f90a7d7808a1
alandoherty95 <alandoherty95@gmail.com>
2021-08-26 16:52:18 +0000

    Updated email_address to email in contact app

--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,7 @@ class ReceivedMessage(models.Model):
         verbose_name = 'Message'
 
     name = models.CharField(max_length=40, null=False, blank=False)
-    email_address = models.EmailField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
     subject = models.CharField(max_length=254, null=False, blank=False)
     message = models.TextField(blank=False, null=False)
     date_sent = models.DateTimeField(auto_now_add=True)
