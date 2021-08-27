dfcc365468cf0695e6b1706fcd1e6b1c4546085b
alandoherty95 <alandoherty95@gmail.com>
2021-08-27 11:07:57 +0000

    Updated gmail settings in settings.py

--- siopa_worldwide/settings.py
+++ siopa_worldwide/settings.py
@@ -218,7 +218,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Confirmation email settings
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'siopaworldwide@example.com'
+    DEFAULT_FROM_EMAIL = 'siopaworldwide@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
